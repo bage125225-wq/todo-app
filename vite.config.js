97294955c3fcc9aa8fda7ee5fedfb1cd2015b0db
yotei.cho <yotei.cho@pr.cri.co.jp>
2025-09-19 10:46:31 +0900

    1

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: './',
+  base: '/todo-app/', // ⚠️ 和你的仓库名一致
   plugins: [vue()]
 })
-
